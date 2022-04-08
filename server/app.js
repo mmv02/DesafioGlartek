eecbe4b751e238a0119aab933d827bef83a2efdf
mmv24608 <marcio.valerio2501@gmail.com>
2022-04-08 02:53:32 +0100

    Resposta do servidor alterada
    O servidor agora responde a um pedido post  por parte do cliente os dados enviados são filtrados para que sejam enviados os dados necessários.

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const express= require('express');
 const cors = require('cors');
 
 var fs = require("fs");
+const { response } = require('express');
 
 const app = express();
 
@@ -14,15 +15,21 @@ app.use(cors());
 
 const router = express.Router();
 
-router.get('/cities',function(req,res) {
+function filtrarDados(Dados){
+    
+    var novaLista = [];
+    
+    for(const key in Dados)
+    {
+        novaLista.push([Dados[key].nome,Dados[key].codigo,Dados[key].data.list]);
+    }    
+    return novaLista;
+}
+
+router.post("/refresh",function(req,res) {
     fs.readFile(__dirname+"/"+"cities.json","utf8", function (err, data){
-        return res.send(data);
+        return res.send(filtrarDados(JSON.parse(data)));
     })
-
-});
-
-router.get("/",function(req,res) {
-    //res.sendFile('../client/public/index.html');
 });
 
 app.use('/',router);
