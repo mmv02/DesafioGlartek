9a9c8ccbbcc59a0ea140cb6362fae6138375ebac
mmv24608 <marcio.valerio2501@gmail.com>
2022-04-19 23:30:12 +0100

    HOT fix Limpeza de código

--- server/app.js
+++ server/app.js
@@ -4,22 +4,11 @@ const cors = require('cors');
 
 var fs = require("fs");
 
-/*
 
-const axios = require("axios").create({baseUrl: "http://api.openweathermap.org/data/2.5/forecast?id=2267056&APPID=3e6291afc1801b5aec1d61a3027111ec"});
-
-Axios.post("http://127.0.0.1:3020/refresh",{
-    message:"Atualizar"
-  }).then((response)=>{
-    setListaDados(response.data);
-  })
-
-*/
 const app = express();
 
 
 app.use(express.json());
-
 
 app.use(cors());
 
@@ -41,35 +30,6 @@ router.post("/refresh",function(req,res) {
         return res.send(filtrarDados(JSON.parse(data)));
     })
 });
-
-
-function atualizarDadosLocais(){
-    var newData = {};
-
-    fs.readFile(__dirname+"/"+"cities.json","utf8", function (err, data){
-        const dados = JSON.parse(data);
-        const identifiers = [dados.Lisboa.codigo, dados.Leiria.codigo,dados.Porto.codigo, dados.Faro.codigo, dados.Coimbra.codigo];
-        
-       
-        for (let index = 0; index < identifiers.length; index++) {
-            const element = identifiers[index];
-            router.get("http://api.openweathermap.org/data/2.5/forecast?id="+element+"&APPID=3e6291afc1801b5aec1d61a3027111ec", (req, res, data) => {
-                newData.push(JSON.parse(data));
-                console.log(res);
-            })
-        }
-
-
-
-
-    })
-    //console.log(newData);
-    //fs.writeFile(__dirname+"/cities.json",JSON.parse(newData));
-
-}
-
-atualizarDadosLocais();
-//
 
 
 
