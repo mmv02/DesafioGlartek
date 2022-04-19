86fda35847b7dbbefde6526e8e28b34d76bdea29
mmv24608 <marcio.valerio2501@gmail.com>
2022-04-19 02:15:34 +0100

    Front-End Finalizado. A receber e a listar os dados da api local.

--- client/src/components/CityRow.js
+++ client/src/components/CityRow.js
@@ -1,36 +1,18 @@
 import React from 'react';
 
 function CityRow(props) {
+  const indice = (props.lista).length-1;
   return (
-    <div className="card">  
-    <div className="card-body">
-      <h4 className="card-title">{props.nome}</h4>
-      <p className="card-text">{props.codigo}</p>
-      <p className="card-text">{(props.temperatura-273.15).toFixed(2)}º C</p>
-      <button type="button" class="btn btn-primary" data-bs-toggle={"#modal"+props.nome} data-bs-target={"#modal"+props.nome}>
-        Ver histórico
-      </button>
-      <div class="modal" id={"modal"+props.nome}>
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h4 class="modal-title">Modal Heading</h4>
-            <button type="button" class="btn-close" data-bs-dismiss={"modal"+props.nome}></button>
-          </div>
-          <div class="modal-body">
-            {props.lista.forEach(element => {
-                return<p>{element.main.temp}</p>
-            })}
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-danger" data-bs-dismiss={"modal"+props.nome}>Close</button>
-          </div>
-
-        </div>
-      </div>
+    <div className="card m-auto w-50  shadow p-4 mb-4 bg-white">  
+      <div className="card-body">
+        <h4 className="card-title">{props.nome}</h4>
+        <p className="card-text text-muted">{props.codigo}</p>
+        <p className="card-text m-0">{(props.lista[indice].main.temp-273.15).toFixed(2)} ºC</p>
+        <p className="card-text">{props.lista[indice].weather[0].main} ({props.lista[indice].weather[0].description}) </p>
+        <p className="card-text">Velocidade do vento: {props.lista[indice].wind.speed} milhas/h </p>
+        <p className="card-text text-muted "><small>Dados de {props.lista[indice].dt_txt}</small> </p>
+      </div> 
     </div>
-    </div>  
-  </div>
   );
 }
 
